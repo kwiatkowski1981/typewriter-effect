9515822adbbb2a3be574bee12bc16cedb6c8d122
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-02-17 17:28:29 +0100

    I used the add letter as interval function where I added the index counter to the letters of the text. interval prints every letter (character, even space) at the time I specify in a variable. I have set the interval to end at the end of the text with the (text) .length method.

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,22 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+
+const spanTxt = document.querySelector('.text');
+const text = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled 
+it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic 
+typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
+containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including 
+versions of Lorem Ipsum.`
+
+let indexText = 0;
+const time = 40;
+const addLetter = () => {
+spanTxt.textContent += text[indexText];
+    indexText++;
+    if (indexText === text.length) clearInterval(indexTyping);
+}
+const indexTyping = setInterval(addLetter, time);
+
